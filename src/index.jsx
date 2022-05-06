3e91626f96a9d07cada250fb2f87bf4cae0e95e5
atuatusann <>
2022-05-06 11:19:14 +0900

    main.jsx delete

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Main } from "./main";
+import { App } from "./App";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Main />
+    <App />
   </React.StrictMode>
 );
